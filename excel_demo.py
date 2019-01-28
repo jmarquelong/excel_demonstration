4220a00c163533edd236dec198f40299b4e95805
Marque <45186181+jmarquelong@users.noreply.github.com>
2019-01-28 00:34:38 -0500

    Add files via upload

--- excel_demo.py
+++ excel_demo.py
@@ -1,25 +1,36 @@
 import openpyxl
 
+# Prompts user to pay attention to case.
 print('All inputs are case sensitive!')
+# Prints blank line for formatting.
+print(" ")
+
+# Load file
 file_name = str(input("Enter a file name: "))
 file_type = '.xlsx'
 concat = file_name + file_type
-
 wb = openpyxl.load_workbook(concat)
 ws = wb.active
 
+
+# List for display_error function.
+command_list = 'modify, review, titles, get, save,'
+sheet_list = [wb.sheetnames]
+# Prompts user for input.
 print('Command list: modify, review, titles, get, save.\n'
       'Remember to save after finishing with your changes.')
-
+print(" ")
 user_input = input("Command: ")
 
-command_list = 'modify', 'review', 'titles', 'save', 'get',
-sheet_list = wb.sheetnames
+# Functions follow:
 
 
 def get_sheet():
+    """User selects sheet to modify."""
+    global ws
     sheet_input = input('Enter sheet name: ')
-    print(wb[sheet_input])
+    ws = wb[sheet_input]
+    print(ws)
 
 
 def review_sheets():
@@ -28,11 +39,11 @@ def review_sheets():
 
 
 def staff_contact():
-    """Enter user input into cells."""
+    """Enter user input into cells using the 'modify' command."""
     phone = 10
     data = input('Enter phone number: ')
-    if phone == data:
-        ws['D4'] = data
+    if int(phone) == len(data):
+        ws['D2'] = data
     elif len(data) != phone:
         display_error()
 
@@ -51,10 +62,12 @@ def worksheet_titles():
 
 
 def display_error():
+    """Function can be used to minimize repeat coding."""
     print('Make a valid entry.')
 
 
 # While loop takes user input and executes functions.
+
 while user_input.lower() != 'q':
     if user_input == 'modify':
         staff_contact()
@@ -67,7 +80,7 @@ while user_input.lower() != 'q':
     elif user_input == 'get':
         get_sheet()
     elif user_input != command_list:
-        print('Please enter a valid command.')
+        display_error()
 
     user_input = input("Command: ")
 print('Good bye!')
